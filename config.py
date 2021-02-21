409100bc8f65c8f267ff1182757c68a3c24fab63
Andrei Rybkin <andreirbkn@gmail.com>
2021-02-21 12:22:30 +0500

    CONFIG template

---
+++ config.py
@@ -0,0 +1,13 @@
+
+notion_token_v2 = ""
+notion_table = ""
+notion_date_prop = "Target Date"
+notion_cal_prop = "Calendar"
+notion_del_prop = "Archive"
+timezone = "Asia/Yekaterinburg"
+days_range =  0
+google_calendar_ids = {
+        "Work": "",
+        "Family": "",
+        #   "NameOfCalendar" : "calendar ID"
+}
